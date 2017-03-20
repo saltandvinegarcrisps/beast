7dcd600ce8299f6305d2306e5466db07add6a966
Kieron <kieron@fastmail.fm>
2017-03-20 10:46:52 +0000

    updates

--- src/Commands/MigrationsList.php
+++ src/Commands/MigrationsList.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Doctrine\DBAL\Connection;
 
-class Migrations extends Command
+class MigrationsList extends Command
 {
     use MigrationTrait;
 
